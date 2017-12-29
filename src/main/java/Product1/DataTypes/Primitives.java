e002b8fd50e653391e8e0beae34773a92cf78370
jamescross91 <james_cross91@hotmail.com>
2017-12-29 15:05:20 +0000

    Primitive examples

---
+++ src/main/java/Product1/DataTypes/Primitives.java
@@ -0,0 +1,17 @@
+package Product1.DataTypes;
+
+public class Primitives {
+    public static void main(String args[]) {
+        int integer = 100;
+        double Double = 3.142;
+        float Float = 3.1414f;
+        long Long = 1000000;
+        boolean Boolean = false;
+        char Char = 'c';
+
+        System.out.println("Integer * Double " + integer * Double);
+        System.out.println("Long + Float " + (Long + Float));
+        System.out.println("Boolean " + Boolean);
+        System.out.println("Char " + Char);
+    }
+}
