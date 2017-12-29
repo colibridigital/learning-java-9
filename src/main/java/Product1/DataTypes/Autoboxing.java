575e21abbc1a6da2419f23f6bbf8551e4189da17
jamescross91 <james_cross91@hotmail.com>
2017-12-29 15:17:49 +0000

    Autoboxing example

---
+++ src/main/java/Product1/DataTypes/Autoboxing.java
@@ -0,0 +1,14 @@
+package Product1.DataTypes;
+
+public class Autoboxing {
+    public static void main(String args[]) {
+        Integer objectInt = 10;
+        int primitiveInt = 20;
+
+        Double objectDouble = 3.1;
+        double primitiveDouble = 1.2;
+
+        System.out.println(objectInt + primitiveInt);
+        System.out.println(objectDouble + primitiveDouble);
+    }
+}
