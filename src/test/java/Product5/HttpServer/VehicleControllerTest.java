4eec56594e0bf714dcdc72713866461717350079
jamescross91 <james_cross91@hotmail.com>
2018-06-22 15:25:22 -0400

    Test example

---
+++ src/test/java/Product5/HttpServer/VehicleControllerTest.java
@@ -0,0 +1,30 @@
+package Product5.HttpServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class VehicleControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() {
+        List<Map<String, String>> response = this.restTemplate.getForObject("http://localhost:" + port + "/vehicles", List.class);
+        assertThat(response.get(0).get("make")).isEqualToIgnoringCase("Ford");
+    }
+}
