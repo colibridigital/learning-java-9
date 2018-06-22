5b9cd24b415c9a4de7d1ef70adbaee6f6259e210
jamescross91 <james_cross91@hotmail.com>
2018-06-22 14:49:38 -0400

    Adding simple vehicle API

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module learning.java {
     requires commons.csv;
     requires transitive java.naming;
     requires jdk.incubator.httpclient;
+    requires spring.web;
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
 }
